894289f7d7ed992d398d0421e946a5baa615d52f
Alex Gustafsson <alex-gustafsson@live.se>
2018-12-24 00:13:35 +0100

    Added option to specify number of items to fetch

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Source = require('./lib/adlibris');
 
 const source = new Source();
-source.search('test').then(console.log);
+source.search('test', {items: 25}).then(x => {
+  console.log(x.length);
+  console.log(x[0]);
+});
