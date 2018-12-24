e4c329955e4fbaceee17a4f884f4d80544e2cf3e
Alex Gustafsson <alex-gustafsson@live.se>
2018-12-24 12:04:26 +0100

    Added product endpoint, improved formatting

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const source = new Source();
 source.search('test', {items: 25}).then(x => {
   console.log(x.length);
   console.log(x[0]);
+  source.fetch(x[2]).then(y => {
+    console.log(y);
+  });
 });
