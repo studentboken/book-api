b617f0d02e3793798a54378d809339c0b08cce96
Alex Gustafsson <alex-gustafsson@live.se>
2018-12-24 12:24:49 +0100

    Added categories

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const Source = require('./lib/adlibris');
 const source = new Source();
 source.search('test', {items: 25}).then(x => {
   console.log(x.length);
-  console.log(x[0]);
-  source.fetch(x[2]).then(y => {
+  console.log(x[3]);
+  source.fetch(x[3]).then(y => {
     console.log(y);
   });
 });
