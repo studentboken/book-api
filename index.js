75b6105f8e3279fe7e56fd700878018e32863c92
Alex Gustafsson <alex-gustafsson@live.se>
2018-12-27 16:58:25 +0100

    Added product page endpoint

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const Source = require('./lib/akademibokhandeln');
 const source = new Source();
 source.search('hej', {items: 20}).then(x => {
   console.log(x.length);
-  console.log(x);
+  console.log(x[0]);
+  source.fetch(x[0]).then(y => {
+    console.log(y);
+  });
 });
