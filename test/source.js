56e12efa887e77a99b529ad16cbf4585b924c4af
Alex Gustafsson <alex-gustafsson@live.se>
2019-03-23 21:49:21 +0100

    Fixed failing test

--- test/source.js
+++ test/source.js
@@ -5,5 +5,5 @@ const Source = require('../lib/source');
 test('Non-implemented source class throws on search', async t => {
   const source = new Source();
   const error = await t.throwsAsync(source.search('foo bar'));
-  t.is('Error fetching results for <foo bar>: Not implemented', error.message);
+  t.is('Error fetching results for <foo bar> with options <>: Not implemented', error.message);
 });
