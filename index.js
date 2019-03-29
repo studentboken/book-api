7f60c0c5645b4fca1546f54a1128c45b0b3505c6
Alex Gustafsson <alex-gustafsson@live.se>
2019-03-29 14:42:37 +0100

    Updated README

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ module.exports = {
   Akademibokhandeln: require('./lib/akademibokhandeln'),
   Adlibris: require('./lib/adlibris')
 };
+
+const Akademibokhandeln = require('./lib/akademibokhandeln');
+const source = new Akademibokhandeln();
+
+source.search('Test Driven Development').then(results => {
+  source.fetch(results[0]).then(results => {
+    console.log(JSON.stringify(results, null, 2));
+  })
+});
