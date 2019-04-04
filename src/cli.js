e39e5224f0d983a9892d1cb73a04c3c5455101a3
Alex Gustafsson <alex-gustafsson@live.se>
2019-04-04 15:21:32 +0200

    Added feature to search multiple queries

--- src/cli.js
+++ src/cli.js
@@ -5,19 +5,12 @@ const fs = require('fs');
 const debug = require('debug')('book-api:app');
 const yargs = require('yargs');
 
-const {search} = require('./convenience-methods');
+const {searchAll} = require('./convenience-methods');
 
 async function main(argv) {
   if (argv.debug)
     require('debug').enable('book-api:*');
-
-  const options = {
-    fetchAll: argv.fetchAll || false,
-    searchAllSources: argv.searchAllSources || false,
-    searchResults: argv.searchResults || 0
-  };
-
-  const books = await search(argv.query, options);
+  const books = await searchAll(argv.query.split(','), argv);
 
   if (argv.output) {
     debug('Writing results to file');
@@ -35,9 +28,13 @@ yargs
   .option('fetch-all', {type: 'boolean', describe: 'Fetch all search results'})
   .option('search-all-sources', {type: 'boolean', describe: 'Search all sources'})
   .option('search-results', {alias: 'n', type: 'number', describe: 'Number of search results to include. Is not guaranteed to be honoured'})
+  .option('parallel-queries', {describe: 'The number of queries to process simultaneously', default: 3})
+  .option('minimum-delay', {describe: 'The minimum number of milliseconds to wait between requests', default: 100})
+  .option('maximum-delay', {describe: 'The maximum number of milliseconds to wait between requests', default: 5000})
   .option('output', {alias: 'o', describe: 'Path to the output file'})
   .option('debug', {alias: 'd', type: 'boolean', describe: 'Run in debugging mode'})
   .example('book-api --help', 'Show this help page')
   .example('book-api "test"')
+  .example('book-api "book a","book b"')
   .recommendCommands()
   .command('$0 <query>', 'Search for the query', () => {}, main).parse();
--- src/convenience-methods.js
+++ src/convenience-methods.js
@@ -6,6 +6,17 @@ const sources = [
 ];
 
 /**
+* Wait asynchronously.
+* @param {Number} min - The minimum wait time.
+* @param {Number} max - The maximum wait time.
+*/
+async function wait(min, max) {
+  await new Promise(resolve => {
+    setTimeout(resolve, Math.max(Math.floor(Math.random() * min), max));
+  });
+}
+
+/**
 * Search asynchronously for a query using sensible source priority.
 * @param {String} query - The query to search for.
 * @param {Object} options - Optional options.
@@ -14,7 +25,13 @@ const sources = [
 * @param {Number} options.searchResults - Number of search results to include. Is not guaranteed to be honoured. Defaults to 0 (predefined).
 * @returns {Array} Array of books.
 */
-async function search(query, options = {fetchAll: false, searchAllSources: false, searchResults: 0}) {
+async function search(query, options = {}) {
+  options = Object.assign({
+    fetchAll: false,
+    searchAllSources: false,
+    searchResults: 0
+  }, options);
+
   const books = [];
 
   for (const Source of sources) {
@@ -40,7 +57,7 @@ async function search(query, options = {fetchAll: false, searchAllSources: false
         else
           books[existingIndex].merge(book);
       } else {
-        book.push(book);
+        books.push(book);
       }
     }
 
@@ -51,6 +68,50 @@ async function search(query, options = {fetchAll: false, searchAllSources: false
   return books;
 }
 
+/**
+* Search asynchronously for a query using sensible source priority.
+* @param {Array} queries - Array of queries to search for.
+* @param {Object} options - Optional options.
+* @param {Boolean} options.fetchAll - Fetch all search results. Defaults to false.
+* @param {Boolean} options.searchAllSources - Search all sources. Defaults to false.
+* @param {Number} options.searchResults - Number of search results to include. Is not guaranteed to be honoured. Defaults to 0 (predefined).
+* @param {Number} options.parallelQueries - The number of queries to process simultaneously.
+* @param {Number} options.minimumDelay - The minimum number of milliseconds to wait between requests.
+* @param {Number} options.maximumDelay - The maximum number of milliseconds to wait between requests.
+* @returns {Array} Array of books.
+*/
+async function searchAll(queries, options = {}) {
+  options = Object.assign({
+    fetchAll: false,
+    searchAllSources: false,
+    searchResults: 0,
+    parallelQueries: 3,
+    minimumDelay: 100,
+    maximumDelay: 5000
+  }, options);
+
+  const books = [];
+
+  const batches = [];
+  for (let i = 0; i < queries.length; i += options.parallelQueries)
+    batches.push(queries.slice(i, i + options.parallelQueries));
+
+  let completedBatches = 0;
+  for (const batch of batches) {
+    debug(`Processing ${batch.length} queries`);
+    const results = await Promise.all(batch.map(queries => search(queries, options)));
+    books.push(...results.flat(2)); /* eslint no-await-in-loop: "off" */
+
+    if (++completedBatches < batches.length) {
+      debug('Waiting between queries');
+      await wait(options.minimumDelay, options.maximumDelay); /* eslint no-await-in-loop: "off" */
+    }
+  }
+
+  return books;
+}
+
 module.exports = {
-  search
+  search,
+  searchAll
 };
